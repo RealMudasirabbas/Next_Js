bc3dd9634c0a6a48cb5443def86d307da46129cf
Mudasir Abbas <mudasirabbas5638201@gmail.com>
2024-06-29 22:36:38 +0500

    message api completed

---
+++ mysterious-feedback/src/app/api/send-message/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/lib/db/dbConnect";
+import UserModel from "@/model/User.model";
+import { Message } from "@/model/User.model";
+
+async function POST(request: Request) {
+    dbConnect();
+
+    const { username, content } = await request.json();
+    try {
+        const user = await UserModel.findOne({ username });
+
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                { status: 404 },
+            );
+        }
+
+        if (!user.isAcceptingMessages) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User is not accepting messages",
+                },
+                { status: 403 },
+            );
+        }
+
+        const newMessage = { content, createdAt: new Date() };
+        user.messages.push(newMessage as Message);
+        await user.save();
+
+        return Response.json(
+            {
+                success: true,
+                message: "message sent successfully",
+            },
+            { status: 201 },
+        );
+    } catch (error) {
+        console.log("Error occured while sending user message", error);
+
+        return Response.json(
+            {
+                success: false,
+                message: "Error occured while sending user message",
+            },
+            { status: 501 },
+        );
+    }
+}
