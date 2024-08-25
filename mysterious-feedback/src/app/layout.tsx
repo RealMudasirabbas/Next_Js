a0c2344010597175663a4ea96cdf4b1d834d2472
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-08-25 18:13:56 +0500

    dashboard done

--- mysterious-feedback/src/app/layout.tsx
+++ mysterious-feedback/src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <html lang="en">
             <AuthProvider>
                 <body className={inter.className}>
+                
                     {children}
                     <Toaster />
                 </body>
