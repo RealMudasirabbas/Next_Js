b06ca7db25c3035de5aa9fa4c07bc7c033785118
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-09-05 15:26:17 +0500

    testing and bug fixing remain

--- stackoverflow-appwrite/src/app/(auth)/layout.tsx
+++ stackoverflow-appwrite/src/app/(auth)/layout.tsx
@@ -1,34 +1,30 @@
 "use client";
+
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { useAuthStore } from "@/store/Auth";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import React from "react";
 
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useAuthStore();
+  const router = useRouter();
 
-function Layout({ children }: { children: React.ReactNode }) {
-    const {session} = useAuthStore();
-    const router = useRouter();
-
-    // if session exists, redirect to home page
-    useEffect(() => {
-
-      if (session) {
-        router.push("/");
-      }
-
-
-    },[session,router])
-
-    // if session exists, hide the layout
+  React.useEffect(() => {
     if (session) {
-      return null
+      router.push("/");
     }
+  }, [session, router]);
 
-    return (
-        <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
-            <div className="relative">{children}</div>
-        </div>
-    ); 
-}
+  if (session) {
+    return null;
+  }
+
+  return (
+    <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
+      <BackgroundBeams />
+      <div className="relative">{children}</div>
+    </div>
+  );
+};
 
 export default Layout;
-
--- stackoverflow-appwrite/src/app/users/[userId]/[userSlug]/questions/page.tsx
+++ stackoverflow-appwrite/src/app/users/[userId]/[userSlug]/questions/page.tsx
@@ -1,47 +1,37 @@
-import { databases, users } from "@/models/server/config";
+import Pagination from "@/components/Pagination";
+import QuestionCard from "@/components/QuestionCard";
 import {
   answersCollection,
   db,
-  voteCollection,
   questionCollection,
+  voteCollection,
 } from "@/models/name";
+import { databases, users } from "@/models/server/config";
+import { UserPrefs } from "@/store/Auth";
 import { Query } from "node-appwrite";
 import React from "react";
-import Link from "next/link";
-import ShimmerButton from "@/components/magicui/shimmer-button";
-import QuestionCard from "@/components/QuestionCard";
-import { UserPrefs } from "@/store/Auth";
-import Pagination from "@/components/Pagination";
-import Search from "./Search";
 
 const Page = async ({
+  params,
   searchParams,
 }: {
-  searchParams: { page?: string; tag?: string; search?: string };
+  params: { userId: string; userSlug: string };
+  searchParams: { page?: string };
 }) => {
   searchParams.page ||= "1";
 
   const queries = [
+    Query.equal("authorId", params.userId),
     Query.orderDesc("$createdAt"),
     Query.offset((+searchParams.page - 1) * 25),
     Query.limit(25),
   ];
-
-  if (searchParams.tag) queries.push(Query.equal("tags", searchParams.tag));
-  if (searchParams.search)
-    queries.push(
-      Query.or([
-        Query.search("title", searchParams.search),
-        Query.search("content", searchParams.search),
-      ])
-    );
 
   const questions = await databases.listDocuments(
     db,
     questionCollection,
     queries
   );
-  console.log("Questions", questions);
 
   questions.documents = await Promise.all(
     questions.documents.map(async (ques) => {
@@ -72,20 +62,7 @@ const Page = async ({
   );
 
   return (
-    <div className="container mx-auto px-4 pb-20 pt-36">
-      <div className="mb-10 flex items-center justify-between">
-        <h1 className="text-3xl font-bold">All Questions</h1>
-        <Link href="/questions/ask">
-          <ShimmerButton className="shadow-2xl">
-            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-              Ask a question
-            </span>
-          </ShimmerButton>
-        </Link>
-      </div>
-      <div className="mb-4">
-        <Search />
-      </div>
+    <div className="px-4">
       <div className="mb-4">
         <p>{questions.total} questions</p>
       </div>
